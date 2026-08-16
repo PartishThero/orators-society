5f0921604f780fe33f07ba9753beee70589affbb
Partish Palkritwar <partish.palkritwar07@gmail.com>
2026-08-16 17:33:19 +0530

    Undid perfomance fix bugs

---
+++ src/data/philosophy.js
@@ -0,0 +1,20 @@
+export const philosophyData = [
+  {
+    number: '01',
+    title: 'THE DISCOURSE',
+    desc: 'We strip away performative rhetoric to isolate the mechanics of persuasion. We build arguments that stand regardless of the speaker.',
+    align: 'text-left',
+    colSpan: 'md:col-span-6',
+    border: 'border-b md:border-b-0 md:border-r border-white/5 pb-16 md:pb-0',
+    delay: 0.3
+  },
+  {
+    number: '02',
+    title: 'THE ARENA',
+    desc: 'Ideas are fragile until tested. We provide the crucible. Every session is an exercise in deconstructing orthodoxy.',
+    align: 'text-left md:text-right',
+    colSpan: 'md:col-span-6 md:pl-16',
+    border: 'pt-16 md:pt-0',
+    delay: 0.4
+  }
+];
